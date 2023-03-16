95a3c3cf72606c6b5cf5eb479c11a7cbac9a90ef
Vitalii Nobis <vitalii.nobis@neatbyte.solutions>
2023-03-16 11:50:55 +0200

    Fix Task1: add letter counting

--- home1/task1.js
+++ home1/task1.js
@@ -3,8 +3,15 @@ function isAnagrams(str1 = "", str2 = "") {
     return false;
   }
 
+  const letterBucket = Array.from(str2);
   return Array.from(str1).every((letter) => {
-    return str2.includes(letter);
+    const index = letterBucket.indexOf(letter);
+    if (index !== -1) {
+      letterBucket.splice(index, 1);
+      return true;
+    } else {
+      return false;
+    }
   });
 }
 
@@ -12,3 +19,4 @@ function isAnagrams(str1 = "", str2 = "") {
 console.log("Positive case: ", isAnagrams("rasp", "spar"));
 console.log("Negative case: ", isAnagrams("rasp", "soap"));
 console.log("Different length: ", isAnagrams("rasp", "candy"));
+console.log("Test: ", isAnagrams("aaab", "abbb"));
